0124a75e39c025db3f721b5357399f4a3a9d5913
JaGeo <janinegeorge.ulfen@gmail.com>
2017-11-22 12:58:44 +0100

    Adapted Example

--- Example/Example.py
+++ Example/Example.py
@@ -1,6 +1,6 @@
 from ContributionsOfAtomsToModes  import AtomicContributionToModes 
 
-
+#Initialize AtomicContributionToModes 
 test=AtomicContributionToModes(PoscarName='POSCAR',ForceConstants=False,ForceFileName='FORCE_SETS',supercell=[[3, 0, 0],[0, 3, 0], [0, 0, 4]])
 
 
@@ -8,34 +8,16 @@ test=AtomicContributionToModes(PoscarName='POSCAR',ForceConstants=False,ForceFil
 test.write_file();
 
 
-#now plots: 
-#grouping of atoms for plots
-grouping={}
+
 #atom numbers start at 1, not at 0
+atomgroups=[[1,2],[3,4],[5,6,7,8],[9,10,11,12,13,14,15,16]] #this list groups the atoms for the plots
+colorofgroups=['black','red','green','grey'] #this list gives the colors for the grouped atoms
+legendforgroups=['C','O','N','H'] #this list gives the legend for the grouped atoms
+
+#Plot with all modes
+test.plot(atomgroups=atomgroups,colorofgroups=colorofgroups,legendforgroups=legendforgroups,filename='allmodes.eps')
+
+#Plot with modes that have the irreducible representation B2 and E (leaves out translational modes by default)
+test.plot_irred(atomgroups=atomgroups,colorofgroups=colorofgroups,legendforgroups=legendforgroups,irreps=['B2','E'],filename='IRactivemodes.eps')
 
 
-
-
-
-atomgroups=[[1,2],[3,4],[5,6,7,8],[9,10,11,12,13,14,15,16]]
-colorofgroups=['black','red','green','grey']
-legendforgroups=['C','O','N','H']
-
-test.plot(atomgroups=atomgroups,colorofgroups=colorofgroups,legendforgroups=legendforgroups)
-test.plot_irred(atomgroups=atomgroups,colorofgroups=colorofgroups,legendforgroups=legendforgroups,irreps=['B2','E'])
-
-#lists all numbers of frequencies that are plotted, frequency numbers start at 1, not at 0
-#freqlist=[1,2,3,4]
-
-#Uebergebe Mulliken Symbole IR-aktiver Moden
-
-#IRactiveModes= [1,3,6,9,11,14,17,19,23,25,27,29,31,33,36,37,40,41,44,45,48]			
-
-#LabelsForIRactiveModes=['E','B2','E','E','E','E','E','B2','E','E','B2','E','B2','E','B2','E','B2','E','B2','E','B2']
-
-
-
-#Symmetrien der einzelnen Moden abfragen und eintragen?
-#Auswahl verschiedener Frequenzen? Einbau ueber Freqrange und eine neue Frequenzliste, die nur noch Frequenzen innerhalb des Bereiches enthaelt 
-#Entartete Moden rauslassen
-#test.plot(grouping=grouping,labelsforfreq=LabelsForIRactiveModes,freqlist=IRactiveModes) #,freqstart=0.0,freqend=150,freqlist=freqlist)
