4e21c80605349532b11ea8a97f0b137a6e4932a2
JaGeo <janinegeorge.ulfen@gmail.com>
2019-06-01 21:48:44 +0200

    Test

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="-JaGeo",
+    version="1.3",
+    author="Janine George",
+    author_email="janine.george@uclouvain.be",
+    description="Package to display atomic contributions to modes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JaGeo/AtomicContributions",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: BSD 3-Clause License ",
+        "Operating System :: OS Independent",
+    ],
+)
